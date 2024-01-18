40e6fe4a6321125effa462f0dc4fa5165f64086d
Nosy Hery Hasina <87439933+Jilo121@users.noreply.github.com>
2024-01-18 13:01:23 +0300

    Modification Contact et Competence

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,20 @@ let project = {
         'detail':["Application mobile: Encyclopedie des histoires des foko Malagasy", "Application web pour la gestion d'une entreprise de construction","Gestion du Centre Medicaux-Sociale du Jirama","Dictionnaire des medicaments et maladies les plus repondus a Madagascar","Envoie d'email automatique pendant une durree limite a une date precise"],
 }
 
-const span = document.querySelector('.insertProj');
+let langage = {
+    'name':["HTML","CSS","Javascript","Python","React Js","React Native","Laravel","Symfony","Photoshop"],
+    'image':["html.png","css.png","js.png","py.png","react.png","react.png","laravel.png","symfony.png","ps.png"]
+}
 
-console.log(span)
+const span = document.querySelector('.insertProj');
+const comp_langage = document.querySelector('.langage_container');
 
 
 for (i=0;i<project.name.length;i++) {
     const div = "<div class='card'><p class='titreproj'>"+ project.name[i] +"</p><p class='descr'>"+ project.description[i] + "</p><p class='detail'>"+ project.detail[i] +"</p>"+ "<img src='./assets/img/git-logo.png' alt='profile image' class='git'>" +"</div>"
-    
-    console.log(div)
-
     span.innerHTML += div
+}
+for (i=0;i<langage.name.length;i++) {
+    const div = "<div class='card'><img src='./assets/img/"+ langage.image[i] +"' alt='"+ langage.image[i] +"' class='lname'><p class='descr'>"+ langage.name[i] + "</p></div>"
+    comp_langage.innerHTML += div
 }
